b8d7e42112163e0a1bdc0386087cb9e065e1fd80
eriyaterada <eriyaterada_2013@depauw.edu>
2013-04-11 19:16:01 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PassGenerator::Application.config.secret_token = '50d6dd11c87c6674beffc74d56c3416ed18fa6120fe5377b282dbe4f8ad8cad642dadc1c3227856d1c8e904d93151ca9ec8919db856d15c1d001da774d645748'
