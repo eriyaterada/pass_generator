4a55fcccb649870446b2d66d03fcd39bdcdbdfac
unknown <Administrator@.(none)>
2013-04-11 23:19:36 -0400

    password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, :presence => true, :format => {:with => email_regex},
             :uniqueness => {:case_sensitive => false}
+
+  attr_accessor :password
+  attr_accessible :email, :password, :password_confirmation
+  passowrd_regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[!\#\$\@\_\+\,\?\[\]])(?!.*["'])/
+  valitates :password, :presence => true, :confirmation => true, :length => {:within =>8...32}, :format => {:with => passowrd_regex}
 end
