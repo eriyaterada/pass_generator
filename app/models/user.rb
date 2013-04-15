2cff6f91a3d6b963aaca2117717675294b0aad68
unknown <Administrator@.(none)>
2013-04-14 22:41:20 -0400

    authentication fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,46 +1,54 @@
 require 'digest'
+
 class User < ActiveRecord::Base
-  attr_accessible :email
-  validates :email, :presence => true, :length => {:maximum => 50} #validate the presence of name and that email address is less than 50 characters long
-
+  attr_accessible :email, :name
+  
+  
+  #VALIDATIONS
+  validates :name, :presence => true #validates presence of name
+  validates :name, :presence => true, :length => {:maximum => 50} 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, :presence => true, :format => {:with => email_regex},
-            :uniqueness => {:case_sensitive => false}
-
-  attr_accessor :password
-  attr_accessible :email, :password, :password_confirmation
-  passowrd_regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[!\#\$\@\_\+\,\?\[\]])(?!.*["'])/
+  validates :email, :presence => true, :format => {:with => email_regex}, :uniqueness => {:case_sensitive => false}
+  
+  #AUTHENTICATION
+  attr_accessor :password #this creates the virtual attributes password
+  attr_accessible :name, :email, :password, :password_confirmation
+   passowrd_regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[!\#\$\@\_\+\,\?\[\]])(?!.*["'])/
   validates :password, :presence => true, :confirmation => true, :length => {:within =>8...32}, :format => {:with => passowrd_regex}
 
-  before_save :encrypted_password
-  def has_password?(submitted_password)
-    encrypted_password == encrypt(submitted_password)
-  end
-  def self.authenticate(email,submitted_password)
-    user = find_by_email(email)
-    return nil if user.nil?
-    return user if user.has_password?(submitted_password)
-  end
-  private
-  def encrypted_password
-     self.encrypted_password = encrypt(password)
-  end
-
-  def encrypt_password
-    self.salt = make_salt unless has_password?(password)
-    self.encrypted_password = encrypt(password)
-  end
   
-  def encrypt(string)
-    secure_hash("#{salt}--#{string}")
-  end
-  def make_salt
-    secure_hash("#{Time.now.utc}--#{password}")
-  end
-  def secure_hash(string)
-    Digest::SHA2.hexdigest(string)
-  end
-  
+  before_save :encrypt_password
+    #this ensures that before you save the password to the databse,
+    #the method encrypt_password is called
+    
+private
+   def encrypt_password
+      self.salt = make_salt unless has_password?(password)
+      self.encrypted_password = encrypt(password)
+   end
+public
+   def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+   end
+
+   def make_salt
+      secure_hash("#Time.now.utc}--#{password}")
+   end
+
+   def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+   end
+
+   def has_password?(submitted_password)
+      encrypted_password == encrypt(submitted_password)
+   end
 
 
+
+   def self.authenticate(email, submitted_password)
+      user = find_by_email(email)
+      return nil if user.nil?
+      return user if user.has_password?(submitted_password)
+   end
+   
 end
