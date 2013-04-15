7bd2f35dbdd8ceb71bdc8be152ed9a91d468d9b8
eriyaterada <eriyaterada_2013@depauw.edu>
2013-04-14 23:06:03 -0400

    fixed user error

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,12 @@ require 'digest'
 
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email
   
   
   #VALIDATIONS
-  validates :name, :presence => true #validates presence of name
-  validates :name, :presence => true, :length => {:maximum => 50} 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, :presence => true, :format => {:with => email_regex}, :uniqueness => {:case_sensitive => false}
+  validates :email, :presence => true, :format => {:with => email_regex}, :uniqueness => {:case_sensitive => false}, :length => {:maximum => 50} 
   
   #AUTHENTICATION
   attr_accessor :password #this creates the virtual attributes password
