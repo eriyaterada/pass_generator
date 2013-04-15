2347ef0726954f3c620ced4e957ebd5b9df1abc3
unknown <Administrator@.(none)>
2013-04-14 20:23:15 -0400

    authentication and encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require 'digest'
 class User < ActiveRecord::Base
   attr_accessible :email
   validates :email, :presence => true, :length => {:maximum => 50} #validate the presence of name and that email address is less than 50 characters long
@@ -9,5 +10,34 @@ class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :email, :password, :password_confirmation
   passowrd_regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[!\#\$\@\_\+\,\?\[\]])(?!.*["'])/
-  valitates :password, :presence => true, :confirmation => true, :length => {:within =>8...32}, :format => {:with => passowrd_regex}
+  validates :password, :presence => true, :confirmation => true, :length => {:within =>8...32}, :format => {:with => passowrd_regex}
+  before_save :encrypted_password
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  def self.authenticate(email,submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+  private
+  def encrypted_password
+     self.encrypted_password = encrypt(password)
+  end
+
+  def encrypt_password
+    self.salt = make_salt unless has_password?(password)
+    self.encrypted_password = encrypt(password)
+  end
+  
+  def encrypt(string)
+    secure_hash("#{salt}--#{string}")
+  end
+  def make_salt
+    secure_hash("#{Time.now.utc}--#{password}")
+  end
+  def secure_hash(string)
+    Digest::SHA2.hexdigest(string)
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130412022554) do
+ActiveRecord::Schema.define(:version => 20130414235757) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
     t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
